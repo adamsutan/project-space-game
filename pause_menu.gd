ba7aa7218b31eca28238e33dad638d8de3d8c0eb
adamsutan <adamsutans@gmail.com>
2025-11-04 21:54:58 +0700

    added pause button. please fix it

---
+++ pause_menu.gd
@@ -0,0 +1,45 @@
+extends Control
+
+func _ready() -> void:
+	$AnimationPlayer.play("RESET")
+
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func testpause():
+	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+		resume()
+
+
+func _on_continue_pressed() -> void:
+	resume()
+
+
+func _on_setting_pressed() -> void:
+	get_tree().change_scene_to_file("res://setting.tscn")
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://main menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _process(delta):
+	testpause()
+
+func _notification(what: int) -> void:
+	match what :
+		Node.NOTIFICATION_PAUSED:
+			hide()
+		Node.NOTIFICATION_UNPAUSED :
+			show()
