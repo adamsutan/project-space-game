3e9afd54e0bc29b487c0a7a4832d7ea58bb7bf70
rafli-raihan <mrafliraihan@proton.me>
2025-11-05 23:16:52 +0700

    lupa :v

--- pause_menu.gd
+++ pause_menu.gd
@@ -4,7 +4,6 @@ func _ready() -> void:
 	$AnimationPlayer.play("RESET")
 	set_visible(false)
 
-
 func resume():
 	set_visible(false)
 	get_tree().paused = false
@@ -21,18 +20,15 @@ func testpause():
 	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
 		resume()
 
-
 func _on_continue_pressed() -> void:
 	resume()
-
 
 func _on_setting_pressed() -> void:
 	get_tree().change_scene_to_file("res://setting.tscn")
 
-
 func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://main menu.tscn")
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
