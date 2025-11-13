bba2a55412faafde1872dfc7601cbcbd6faa8d46
rafli-raihan <mrafliraihan@proton.me>
2025-11-13 23:39:49 +0700

    enemie health, hitbox. bullet hurtbox, culling

--- bullet.gd
+++ bullet.gd
@@ -11,3 +11,17 @@ func _ready():
 func _physics_process(delta):
 	velocity=Vector2(speed,0).rotated(dir)
 	move_and_slide()
+
+func _on_timer_timeout() -> void:
+	queue_free() # Replace with function body.
+
+
+# Tambahkan fungsi ini untuk menangani tabrakan
+func _on_body_entered(body):
+	# Bisa tambahkan logika, misalnya cek apakah body adalah musuh
+	queue_free()
+
+
+func _on_hurtbox_body_entered(body: Node2D) -> void:
+	if body.scene_file_path == "res://enemy.tscn":
+		queue_free()# Replace with function body.
--- enemy.gd
+++ enemy.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
+@onready var health_info: Label = $HealthInfo
+
 var speed = 40
 var player = null
 var player_chase = false
+var _health = 100
+
+func _ready() -> void:
+	health_info.text = str(_health)
 
 func _physics_process(delta: float) -> void:
 	if player_chase:
@@ -13,11 +19,19 @@ func _physics_process(delta: float) -> void:
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	# Cek apakah nama scene-nya "ship" atau filename-nya "ship.tscn"
-	if body.name == "ship" or body.scene_file_path == "res://ship.tscn":
+	if body.name == "Ship" or body.scene_file_path == "res://ship.tscn":
 		player = body
 		player_chase = true
-	
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
-	player = null
-	player_chase = false
+	if body.name == "Ship" or body.scene_file_path == "res://ship.tscn":
+		player = null
+		player_chase = false
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.name == "Bullet" or body.scene_file_path == "res://bullet.tscn":
+		_health -= 20
+		health_info.text = str(_health)
+		
+		if _health <= 0:
+			queue_free()
